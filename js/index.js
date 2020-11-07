9fcc9b8a958eed9f22584f2ce32422470478beb3
Abdulla <hereabdulla@gmail.com>
2020-11-07 09:35:19 +0000

    update search

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// window.addEventListener('load',() =>{
+		
+// })
+function open_job_detail(id){
+    console.log("Test")
+    // var t = document.getElementById("ts")
+    localStorage.setItem("selected_job",id)
+}
+
+function open_browse_job(id){
+    console.log("browse")
+    console.log(id)
+    // var t = document.getElementById("ts")
+    // localStorage.setItem("selected_job",id)
+}
